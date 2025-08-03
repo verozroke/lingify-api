45c90481169f4399d631fe2ad7b5b6f672aa39ed
Askhat <73778760+verozroke@users.noreply.github.com>
2025-08-04 00:14:01 +0500

    test: fix prisma path resolution

--- src/card-sets/card-sets.controller.spec.ts
+++ src/card-sets/card-sets.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { CardSetsController } from "./card-sets.controller";
 import { CardSetsService } from "./card-sets.service";
+import { PrismaService } from "prisma/prisma.service";
 
 describe("CardSetsController", () => {
   let controller: CardSetsController;
@@ -8,7 +9,7 @@ describe("CardSetsController", () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CardSetsController],
-      providers: [CardSetsService],
+      providers: [CardSetsService, { provide: PrismaService, useValue: {} }],
     }).compile();
 
     controller = module.get<CardSetsController>(CardSetsController);
